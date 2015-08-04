2097b642e45a7b64c2e48fbc52bfad1016796943
Noah Xiao <noah.xm.shaw@gmail.com>
2015-08-04 17:00:49 +1000

    Update ngram.R

--- ngram.R
+++ ngram.R
@@ -1,7 +1,8 @@
 ##############################################
 ## 0. Set up #################################
 ##############################################
-
+require(stringi)
+require(data.table)
 ## 0.1 load the required libraries and clean #
 ## up the memory #############################
 rm(list = ls())
@@ -16,19 +17,52 @@ load("data/RData/freq.RData")
 ## 1. Ngram table ############################
 ##############################################
 
-tbBigram <- matrix(nrow = length(freqUni), ncol = length(freqUni))
-i <- 1
-j <- 1
-for (i in 1:length(freqUni)) {
-    for (j in 1:length(freqUni)) {
-        gram <- paste(names(freqUni[i]), names(freqUni[j]))
-        
-        freqUp <- freqBi[gram]
-        freqDown <- freqUp[j]
-        
-        if (is.null(freqUp)) {
-            freqUp <- 0
-        }
-        tbBigram[i, j] <-  freqUp / freqDown
-    }
+## below is too slow
+# tbBigram <- matrix(nrow = length(freqUni), ncol = length(freqUni))
+# i <- 1
+# j <- 1
+# for (i in 1:length(freqUni)) {
+#     for (j in 1:length(freqUni)) {
+#         gram <- paste(names(freqUni[i]), names(freqUni[j]))
+#         
+#         freqUp <- freqBi[gram]
+#         freqDown <- freqUp[j]
+#         
+#         if (is.null(freqUp)) {
+#             freqUp <- 0
+#         }
+#         tbBigram[i, j] <-  freqUp / freqDown
+#     }
+# }
+
+## 1.1 create a function to extract elements #
+## in a gram #################################
+NgramTb <- function (mxFreq) {
+  ## Use: return a ngram probability table
+  ## Args: mxFreq: a ngram frequency matrix
+  ## Return: a ngram probability table
+  
+  ## determins the n of the ngram
+  n <- length(unlist(gregexpr(" ", names(mxFreq)[1]))) + 1
+  ## substring the grams into 2 parts, last word and the rest words
+  listWords <- stri_split_fixed(names(mxFreq), " ")
+  vecLastWord <- sapply(listWords, function (x) unlist(x)[n])
+  vecRestWowrds <- sapply(listWords, function (x) paste(unlist(x)[-n], collapse = " "))
+  ## build a data table
+  tbGram <- data.table("ngram" = names(mxFreq)
+                       , "freq" = mxFreq
+                       , "x" = vecRestWowrds
+                       , "y" = vecLastWord)
+  
+  return(tbGram)
 }
+tbUnigram <- NgramTb(freqUni)
+tbBigram <- NgramTb(freqBi)
+tbTrigram <- NgramTb(freqTri)
+tbQuatrgram <- NgramTb(freqQuatr)
+
+
+
+  
+  
+  
