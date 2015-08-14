6ad296c49b0209d511e2333aa63469ebfb8efea1
Noah Xiao <noah.xm.shaw@gmail.com>
2015-08-14 16:54:05 +1000

    Update server.R

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -6,45 +6,87 @@
 ## up the memory #############################
 require(shiny)
 ## 0.2 set up the working directory ##########
-setwd("/Volumes/Data Science/Google Drive/learning_data_science/Coursera/capstone/")
+# setwd("/Volumes/Data Science/Google Drive/learning_data_science/Coursera/capstone/")
 
 ##############################################
 ## 1. Server #################################
 ##############################################
 function(input, output, session) {
     terms <- reactive({
-        # change when
-        withProgress({
-            setProgress(message = "initializing ...")
-            tbPred <- PredGram(input$input)
-
-            tbPred
-        })
+        if (gsub("\\s", "", input$input) != ""){
+            withProgress({
+                setProgress(message = "initializing ...")
+                tbPred <- PredGram(input$input)
+                
+                tbPred
+            })
+        }
+    })
+    
+    # Make the wordcloud drawing predictable during a session
+    wordcloud_rep <- repeatable(wordcloud)
+    
+    output$rankgraph <- renderPlot({
+        if (gsub("\\s", "", input$input) != ""){
+            RankingChart(tb = terms())
+        }
+    })
+    
+    output$wordcloud <- renderPlot({
+        if (gsub("\\s", "", input$input) != ""){
+            v <- terms()
+            pal <- brewer.pal(8, "Dark2")
+            wordcloud_rep(words = v[, 1]
+                          , freq = v[, 2]
+                          , scale = c(7.5, 1)
+                          , random.order = T
+                          , rot.per = .15
+                          , colors = pal)
             
+            withProgress(
+                message = "rendering wordcloud ..."
+                , value = .1
+                ,  {
+                    for (i in 1:10) {
+                        # Each time through the loop, add another row of data. This a stand-in
+                        # for a long-running computation.
+                        
+                        # Increment the progress bar, and update the detail text.
+                        incProgress(amount = .5, detail = paste(i, "in 10 ... Done"))
+                        
+                        # Pause for 0.1 seconds to simulate a long computation.
+                        Sys.sleep(.1)
+                    }
+                }
+                
+            )
+        }
     })
     
     observe({
-        v <- terms()
-        updateTextInput(session = session
-                        , inputId = "outputGuess1"
-                        , value = v[, 1][1]
-        )
-        updateTextInput(session = session
-                        , inputId = "outputGuess2"
-                        , value = v[, 1][2]
-        )
-        updateTextInput(session = session
-                        , inputId = "outputGuess3"
-                        , value = v[, 1][3]
-        )
-        updateTextInput(session = session
-                        , inputId = "outputGuess4"
-                        , value = v[, 1][4]
-        )
-        updateTextInput(session = session
-                        , inputId = "outputGuess4"
-                        , value = v[, 1][5]
-        )
+        if (gsub("\\s", "", input$input) != ""){
+            v <- terms()
+            updateTextInput(session = session
+                            , inputId = "outputGuess1"
+                            , value = v[, 1][1]
+            )
+            updateTextInput(session = session
+                            , inputId = "outputGuess2"
+                            , value = v[, 1][2]
+            )
+            updateTextInput(session = session
+                            , inputId = "outputGuess3"
+                            , value = v[, 1][3]
+            )
+            updateTextInput(session = session
+                            , inputId = "outputGuess4"
+                            , value = v[, 1][4]
+            )
+            updateTextInput(session = session
+                            , inputId = "outputGuess4"
+                            , value = v[, 1][5]
+            )
+        }
     })
     
     observe({
@@ -95,41 +137,5 @@ function(input, output, session) {
                             , value = gsub("\\s+", " ", paste(input$input, input$outputGuess5))
             )
         })
-    })
-    
-    # Make the wordcloud drawing predictable during a session
-    wordcloud_rep <- repeatable(wordcloud)
-    
-    output$rankgraph <- renderPlot({
-        RankingChart(tb = terms())
-    })
-    
-    output$wordcloud <- renderPlot({
-        v <- terms()
-        pal <- brewer.pal(8, "Dark2")
-        wordcloud_rep(words = v[, 1]
-                      , freq = v[, 2]
-                      , scale = c(4, .5)
-                      , random.order = T
-                      , rot.per = .15
-                      , colors = pal)
-        
-        withProgress(
-            message = "rendering wordcloud ..."
-            , value = .1
-            ,  {
-                for (i in 1:10) {
-                    # Each time through the loop, add another row of data. This a stand-in
-                    # for a long-running computation.
-                    
-                    # Increment the progress bar, and update the detail text.
-                    incProgress(amount = .5, detail = paste(i, "in 10 ... Done"))
-                    
-                    # Pause for 0.1 seconds to simulate a long computation.
-                    Sys.sleep(.1)
-                }
-            }
-            
-        )
     })
 }
