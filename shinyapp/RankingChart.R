4806cb27ce62f1d1c3b9510433ae8174304bf9fc
Noah Xiao <noah.xm.shaw@gmail.com>
2015-08-14 16:54:24 +1000

    Update RankingChart.R

--- shinyapp/RankingChart.R
+++ shinyapp/RankingChart.R
@@ -3,10 +3,11 @@ require(scales)
 RankingChart <- function(tb){
     tb$y <- factor(tb$y, levels = tb$y[order(tb$prob, - rank(tb$y))])
     g <- ggplot(tb, aes(x = y, y = prob))
-    g <- g + geom_bar(stat = "identity", fill = "skyblue2")
-    g <- g + xlab("predictions")
-    g <- g + scale_y_continuous(name = "prob.", labels = percent)
-    g <- g + theme_bw()
-    g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    g <- g + geom_bar(stat = "identity", fill = "skyblue2", width = .5)
+    g <- g + xlab("")
+    g <- g + scale_y_continuous(name = "", labels = percent)
+    g <- g + theme(panel.background = element_rect(fill = "white")
+                   , plot.background = element_rect(fill = "white"))
+    
     last_plot() + coord_flip()
 }
